8a0f9ef4c307f906f4e73c27318d0e8244ceee4d
Evan Wills <evan.i.wills@gmail.com>
2021-05-10 07:52:16 +1000

    started creating the skeleton for the state management.

---
+++ js/state/firing-programs/programs.reducer.state.js
@@ -0,0 +1,35 @@
+export const initialPrograms = [{
+  id: 1,
+  controllerProgramID: '1',
+  type: 'bisque',
+  name: 'Slow bisque',
+  description: 'Good for large work, or small work that\'s not completely dry',
+  steps: [
+    {
+      id: 1,
+      endTemp: 200,
+      rate: 50,
+      hold: 0
+    },
+    {
+      id: 2,
+      endTemp: 520,
+      rate: 100,
+      hold: 0
+    },
+    {
+      id: 3,
+      endTemp: 600,
+      rate: 80,
+      hold: 0
+    },
+    {
+      id: 4,
+      endTemp: 1000,
+      rate: 150,
+      hold: 10
+    }
+  ],
+  created: '2021-05-06T21:13:25+1000'
+  createdBy: 'evanwills'
+}]
---
+++ js/state/kilns/kilns.reducers.state.js
@@ -0,0 +1,17 @@
+export const initialKilnState = [{
+  id: 'woodrow1',
+  brand: 'woodrow',
+  model: '',
+  type: 'general',
+  energy: 'electric',
+  maxTemp: 1260
+  height: 450,
+  depth: 450,
+  width: 450,
+  isWorking: true,
+  isInUse: true,
+  isHot: false,
+  glaze: true,
+  bisque: true,
+  singleFire: false
+}]
